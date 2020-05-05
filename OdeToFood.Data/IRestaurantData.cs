7885299cfa670a8b7830ed591f58137d4b9f1f8c
tylerdav <tdavis1433@att.net>
2020-05-05 10:24:35 -0500

    first commit

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,32 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant {Id = 1, name = "Scott's Pizza", Location= "Maryland", CuisineType=CuisineType.Italian },
+                new Restaurant {Id = 1, name = "Cinnamon Club", Location= "Tennessee", CuisineType=CuisineType.Italian },
+                new Restaurant {Id = 1, name = "La Costa", Location= "Florida", CuisineType=CuisineType.Italian },
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in restaurants
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
